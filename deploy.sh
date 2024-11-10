362b87f2c1331a211438cd6fea956ba615e6702f
zhouyunfu <1301964096@qq.com>
2024-11-10 11:07:37 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -14,31 +14,31 @@ my_variable=$(git commit -m '更新')
 # 使用变量，在变量名前加上$
 echo $my_variable
 
-if [ $commitResult ]
+if [ $my_variable ]
 then
 echo "提交成功"
 fi
 echo "提交失败"
 
-git push origin gh-pages
+#git push origin gh-pages
 
 # 生成静态文件
-npm run build
+#npm run build
 
 # 进入生成的文件夹
-cd public
+#cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:zhouyunfu111/zhouyunfu111.github.io.git master
+#git push -f git@github.com:zhouyunfu111/zhouyunfu111.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:zhouyunfu111/zhouyunfu111.github.io.git master:gh-pages
 
-cd -
+#cd -
